395b793a6bf9a91d09d01404b8b68a6470f566dc
renwenhao <renwenhao@sobey.com>
2018-06-20 13:48:34 +0800

    react+antd基础配置

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import app1 from './app.less';
-import {Button} from 'antd';
+import {Button ,Input } from 'antd';
 import { relativeTimeThreshold } from 'moment';
 console.log('app1',app1);
-
-ReactDom.render(<div><Button type="primary" onClick={()=>{
-    console.log(this);
-}}>HYDL</Button><h1 className={app1.h1}>react write once</h1></div>,document.getElementById('app'));
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div className={app1.h1}>
+            <Button type="primary">按钮</Button>
+            <Button type="primary">按钮</Button>
+            <Button type="primary">按钮</Button>
+            <Button type="primary">按钮</Button>
+            <Button type="primary">按钮</Button>
+            <Input style={{'width':'200px'}}/>
+        </div>
+    }
+}
+ReactDom.render(<App/>,document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 var path = require('path');
-
+var webpack = require('webpack');
+var HTMLPlugin = require('html-webpack-plugin')
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var extractPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry:{
-        app:'./src/app.js'
+        app:'./src/app.js',
+        vendor:['react','react-dom','antd']
     },
     output:{
         path:path.resolve(__dirname,'./dist'),
@@ -27,32 +31,69 @@ module.exports = {
             },
             {
                 test:/\.css$/,
-                use:[
-                    {
+                use:extractPlugin.extract({
+                    fallback:{
                         loader:'style-loader'
                     },
-                    {
-                        loader:'css-loader'
-                    }
-                ]
+                    use:[
+                    
+                        {
+                            loader:'css-loader'
+                        }
+                    ]
+                })
             },
             {
                 test:/\.less/,
-                use:[
-                    {
+                use:extractPlugin.extract({
+                    fallback: {
                         loader:'style-loader'
                     },
-                    {
-                        loader:'css-loader',
-                        options:{
-                            modules:true
+                    use:[
+                   
+                        {
+                            loader:'css-loader',
+                            options:{
+                                modules:true,
+                                minimize:true
+                            }
+                        },
+                        {
+                            loader:'less-loader'
                         }
-                    },
-                    {
-                        loader:'less-loader'
-                    }
-                ]
+                    ]
+                })
             }
         ]
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),//热更相关插件
+        new HTMLPlugin({
+            title:'react+antd',
+            template:'index.html',
+            filename: 'index.html',
+            minify:true
+            // chunks:['app.bundle','vendor.bundle']
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'vendor',
+            minChunks:Infinity
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'common',
+            minChunks:2
+        }),
+        new UglifyJSPlugin(),
+        new extractPlugin("styles.css")
+    ],
+    devServer:{
+        contentBase:"/dist",
+        hot:true,
+        port:"8080",
+        open:true,
+        overlay:{
+            errors:true
+        }
     }
 }
